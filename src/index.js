18d81d305335b4d7a3c9ecf727ad740821ded61a
lichengyin <welefen@gmail.com>
2016-05-12 12:00:14 +0800

    Update

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/**
+ * stc plugin abstract class
+ */
+export default class {
+  /**
+   * constructor
+   */
+  constructor(file){
+    this.file = file;
+  }
+  /**
+   * run
+   */
+  run(){
+    
+  }
+}
