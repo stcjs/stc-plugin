7ec19e7d6f311536250c9b3f204b88d4bf2e391f
lichengyin <welefen@gmail.com>
2016-05-26 09:53:25 +0800

    Add getContent, setContent, getAst, setAst method to plugin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {asyncReplace} from 'stc-helper';
+import {isMaster} from 'cluster';
 
 /**
  * stc plugin abstract class
@@ -7,8 +8,69 @@ export default class {
   /**
    * constructor
    */
-  constructor(file){
+  constructor(file, options = {}, config, cluster){
     this.file = file;
+    this.options = options;
+    this.config = config;
+    this.cluster = cluster;
+  }
+  /**
+   * get file content
+   */
+  getContent(encoding){
+    if(isMaster){
+      return this.file.getContent(encoding);
+    }
+    return this.cluster.invoke({
+      method: 'getContent',
+      args: [encoding],
+      file: this.file.path,
+      options: this.options
+    });
+  }
+  /**
+   * set file content
+   */
+  setContent(content){
+    if(!isMaster){
+      throw new Error('setContent method must be invoked in master');
+    }
+    this.file.setContent(content);
+    return this;
+  }
+  /**
+   * get file ast
+   */
+  async getAst(){
+    if(isMaster){
+      if(this.file.hasAst()){
+        return this.file.getAst();
+      }
+      //get ast in worker parsed
+      let ret = await this.cluster.doTask({
+        type: 'getAst',
+        content: this.getContent('utf8'),
+        file: this.file.path
+      });
+      this.file.setAst(ret);
+      return ret;
+    }
+    //get ast from master
+    return this.cluster.invoke({
+      method: 'getAst',
+      file: this.file.path,
+      options: this.options
+    })
+  }
+  /**
+   * set ast
+   */
+  setAst(ast){
+    if(!isMaster){
+      throw new Error('setAst must be invoked in master');
+    }
+    this.file.setAst(ast);
+    return this;
   }
   /**
    * async content replace
@@ -22,10 +84,10 @@ export default class {
   run(){
     
   }
-  static include(){
-    
-  }
-  static exclude(){
+  /**
+   * update
+   */
+  update(){
     
   }
 }
